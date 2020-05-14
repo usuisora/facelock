2c760c764291b17b5b5b715d1a1b07455dd67c2d
Bohdan Kalynovskyi <bohdan.kalynovskyi@sigma.software>
2020-05-14 03:08:51 +0300

    route const added

---
+++ client/src/constants/routes.ts
@@ -0,0 +1,25 @@
+import { IRoute } from '../types/types';
+
+const routes: IRoute[] = [
+	{
+		name: 'Login',
+		path: '/login'
+	},
+	{
+		name: 'Terminal',
+		path: '/terminal'
+	},
+	{
+		name: 'Settings',
+		path: '/settings'
+	},
+	{
+		name: 'Workers',
+		path: '/workers'
+	},
+	{
+		name: 'Add Worker',
+		path: '/addWorker'
+	}
+];
+export default routes;
